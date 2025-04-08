4b91efbcbdc7037aec5e6aa1f7a5998b2bdd385d
Moabe <moabeluis2018@gmail.com>
2025-04-08 16:58:58 -0300

    Adicionando dados do Cluster MongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,13 @@
 import express from "express";
-
-import authRoutes from './routes/auth.routes.js/'
+import authRoutes from './routes/auth.routes.js'
+import { ENV_VARS } from "./config/envVars.js";
 
 const app = express();
+const PORT = ENV_VARS.PORT
 
 app.use("/api/v1/auth", authRoutes)
 
-app.listen(5000, () => {
-    console.log("Server iniciado: http://localhost:5000");
+app.listen(PORT, () => {
+    console.log(`Server iniciado: http://localhost:${PORT}`);
     console.log("OK")
 })

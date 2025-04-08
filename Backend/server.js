1737295ed0ff3ca59bd89d90d9947c481236e17b
Moabe <moabeluis2018@gmail.com>
2025-04-08 13:48:03 -0300

    Ligando o server na porta 5000

---
+++ Backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Server OK!")
+})
+
+app.listen(5000, () => {
+    console.log("Server iniciado: http://localhost:5000");
+    console.log("OK")
+})

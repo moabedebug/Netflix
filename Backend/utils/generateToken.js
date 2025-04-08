88c1421d011a45bc9ad9cf52d464b71d26ae75da
Moabe <moabeluis2018@gmail.com>
2025-04-08 19:31:47 -0300

    fix(import): adicionar extensão .js no caminho do envVars

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken"
 
-import { ENV_VARS } from "../config/envVars"
+import { ENV_VARS } from "../config/envVars.js"
 
 export const generateTokenAndSetCookie =  (userId, res)  => {
     const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, { expiresIn: "15d" })

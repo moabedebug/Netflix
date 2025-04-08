91f08924fd10423f820d18679867500ad18c19ad
Moabe <moabeluis2018@gmail.com>
2025-04-08 14:08:47 -0300

    Retirando as rotas de server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,10 @@
 import express from "express";
 
+import authRoutes from './routes/auth.routes.js/'
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Server OK!")
-})
+app.use("/api/v1/auth", authRoutes)
 
 app.listen(5000, () => {
     console.log("Server iniciado: http://localhost:5000");

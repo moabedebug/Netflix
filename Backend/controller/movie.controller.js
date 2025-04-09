cfd8650ef3bc2ddea1b0b300d7064e49f26b6b71
Moabe <moabeluis2018@gmail.com>
2025-04-09 11:50:21 -0300

    feat: criar controllers e rota para detalhe dos filmes do TMDB

--- Backend/controller/movie.controller.js
+++ Backend/controller/movie.controller.js
@@ -2,7 +2,7 @@ import { fetchFromTMDB } from "../services/tmdb.services.js"
 
 export async function getTrendingMovie(req, res){
     try{ 
-        const data = await fetchFromTMDB("https://api.themoviedb.org/3/trending/movie/day?language=en-US")
+        const data = await fetchFromTMDB("https://api.themoviedb.org/3/trending/movie/day?language=pt-BR")
         const randomMovie = data.results[Math.floor(Math.random() * data.results?.length)]
         
         res.json({ content: randomMovie })
@@ -16,9 +16,24 @@ export async function getTrendingMovie(req, res){
 export async function getMovieTrailers(req, res) {
     const { id } = req.params;
     try{
-        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`)
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/videos?language=pt-BR`)
         res.json({ Trailer: data.results })
     }catch (error){
+        if(error.message.includes("404")){
+            return res.status(404).send(null)
+        }
+
+        res.status(500).json({ message: "Erro interno do servidor" })
+    }
+}
+
+
+export async function getMovieDetails(req, res) {
+    const { id } = req.params
+    try{
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}?language=pt-BR`)
+        res.status(200).json({ content: data })
+    }catch(error) {
         if(error.message.includes("404")){
             return res.status(404).send(null)
         }

c7512548fcad7e7c4fb73b5ac595044e73e99a0d
Moabe <moabeluis2018@gmail.com>
2025-04-10 17:22:35 -0300

    feat(search): adiciona rotas de pesquisa com person, tv, movies e remove pesquisas

---
+++ Backend/controller/search.controller.js
@@ -0,0 +1,119 @@
+import { User } from "../models/user.model.js"
+import { fetchFromTMDB } from "../services/tmdb.services.js"
+
+export async function searchPerson(req, res) {
+    const { query }= req.params;
+    try{
+        const response = await fetchFromTMDB(
+            `https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=pt-BR&page=1`
+        )
+
+        if(response.results.length === 0){
+            return res.status(404).send(null)
+        }
+
+        await User.findByIdAndUpdate(req.user._id, {
+            $push: {
+                searchHistory:{
+                    id: response.results[0].id,
+                    image: response.results[0].profile_path,
+                    title: response.results[0].name,
+                    searchType: "person",
+                    createdAt: new Date(),
+                }
+            }
+        })
+
+        return res.status(200).json({ content: response.results })
+
+    }catch(error){
+        console.log("Error no searchPerson controller", error.message)
+        res.status(500).json({ message: "Error interno no servidor" })
+    }
+}
+
+export async function searchMovie(req, res) {
+    const { query }= req.params
+    try{
+        const response = await fetchFromTMDB(
+            `https://api.themoviedb.org/3/search/movie?query=${query}&language=pt-BR&page=1`
+        )
+
+        if(response.results.length === 0){
+            return res.status(404).send(null)
+        }
+
+        await User.findByIdAndUpdate(req.user._id, {
+            $push: {
+                searchHistory:{
+                    id: response.results[0].id,
+                    image: response.results[0].poster_path,
+                    title: response.results[0].name,
+                    searchType: "movie",
+                    createdAt: new Date(),
+                }
+            }
+        })
+
+        return res.status(200).json({ content: response.results })
+    }catch(error){
+        console.log("Error no searchMovie controller", error.message)
+        res.status(500).json({ message: "Error interno no servidor" })
+    }
+}
+
+export async function searchTv(req, res) {
+    const { query } = req.params
+    try{
+        const response = await fetchFromTMDB(
+            `https://api.themoviedb.org/3/search/tv?query=${query}&language=pt-BR&page=1`
+        )
+
+        if(response.results.length === 0){
+            return res.status(404).send(null)
+        }
+
+        await User.findByIdAndUpdate(req.user._id, {
+            $push: {
+                searchHistory:{
+                    id: response.results[0].id,
+                    image: response.results[0].poster_path,
+                    title: response.results[0].name,
+                    searchType: "tv",
+                    createdAt: new Date(),
+                }
+            }
+        })
+
+        return res.status(200).json({ content: response.results })
+    }catch(error){
+        console.log("Error no searchTv controller", error.message)
+        res.status(500).json({ message: "Error interno no servidor" })
+    }
+}
+
+export async function getSearchHistory(req, res) {
+    try{
+        res.status(200).json({ content: req.user.searchHistory })
+    } catch(error){
+        res.status(500).json({ message: "Error interno no servidor" })
+    }
+}
+
+export async function removeItemFromSearchHistory(req, res) {
+    let { id } = req.params
+
+    id = parseInt(id)
+
+    try{
+        await User.findByIdAndUpdate(req.user._id, {
+            $pull: {
+                searchHistory: { id: id }
+            }
+        })
+
+        res.status(500).json({ message: "Item removido do histórico de pesquisa" })
+    }catch(error){
+        res.status(500).json({ message: "Error interno no servidor" })
+    }
+}
--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from './routes/auth.routes.js'
 import movieRoutes from './routes/movie.routes.js'
 import tvRoutes from './routes/tv.routes.js'
+import searchRoutes from './routes/search.routes.js'
 
 import { protectRoute } from "./middleware/protectRoute.js";
 
@@ -19,6 +20,7 @@ app.use(cookieParser())
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/movie", protectRoute, movieRoutes)
 app.use("/api/v1/tv", protectRoute, tvRoutes)
+app.use("/api/v1/search", protectRoute, searchRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server iniciado: http://localhost:${PORT}`);

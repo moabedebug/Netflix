62fd6b51565599a39f92b8178bbb0fb6d07d8ad2
Moabe <moabeluis2018@gmail.com>
2025-04-14 15:26:12 -0300

    feat(auth): adiciona função de signup com toast e loading state na store

--- Frontend/src/store/authUser.js
+++ Frontend/src/store/authUser.js
@@ -1,8 +1,21 @@
+import axios from "axios"
+import toast from "react-hot-toast"
 import {create} from "zustand"
 
 export const useAuthStore = create((set) => ({
     user: null,
-    signup: async () => {},
+    isSigninUp: false,
+    signup: async (credentials) => {
+        set({isSigninUp:true})
+        try{
+            const response = await axios.post("/api/v1/auth/signup", credentials)
+            set({user:response.data.user, isSigninUp:false})
+            toast.success("Conta criada com sucesso")
+        }catch(error){
+            toast.error(error.response.data.message || "Ocorreu um erro")
+            set({isSigninUp:false, user:null})
+        }
+    },
     login: async () => {},
     logout: async () => {},
     authCheck: async () => {}

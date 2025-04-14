eab8260ee1b40dba21ed29388700dfe9304d837b
Moabe <moabeluis2018@gmail.com>
2025-04-14 16:17:28 -0300

    feat(app): adiciona chamada à função authCheck no useEffect para verificar autenticação ao iniciar o app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/home/HomePage";
@@ -6,18 +7,25 @@ import SignUpPage from "./pages/SignUpPage"
 
 import { Footer } from "./components/Footer";
 import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/authUser";
 
 export default function App() {
-    return (
-      <>
-      <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/signup" element={<SignUpPage/>} />
-      </Routes>
-      <Toaster/>
-      <Footer/>
-      </>
-    )
-  }
+  const { user, authChekingAuth, authCheck } = useAuthStore()
+  console.log("Auth user is here", user)
+
+  useEffect(() => {
+    authCheck()
+  },[])
   
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/login" element={<LoginPage/>} />
+      <Route path="/signup" element={<SignUpPage/>} />
+    </Routes>
+    <Toaster/>
+    <Footer/>
+    </>
+  )
+}

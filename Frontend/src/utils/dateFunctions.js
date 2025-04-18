f4074a6ed775185d5a238b8fd7c0ef030ae0ac6f
Moabe <moabeluis2018@gmail.com>
2025-04-18 20:43:50 -0300

    fix(utils): adiciona validação e formatação ao formatDate

--- Frontend/src/utils/dateFunctions.js
+++ Frontend/src/utils/dateFunctions.js
@@ -5,3 +5,15 @@ export function formatReleaseDate(date){
       day: "numeric"
     })
 }
+
+export function formatDate(DateString) {
+  const date = new Date(DateString)
+
+  const monthNames =  ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+  const year = date.getUTCFullYear()
+  const month = monthNames[date.getUTCMonth()]
+  const day = date.getUTCDate()
+
+  return `${day} ${month} ${year}`
+}

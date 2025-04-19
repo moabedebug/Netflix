6489a3be62445d77b34862cf34b7f2388a133dd6
Moabe <moabeluis2018@gmail.com>
2025-04-18 21:44:55 -0300

    feat(404): Implementa página NotFound

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,6 +8,7 @@ import SignUpPage from "./pages/SignUpPage"
 import WatchPage from "./pages/WatchPage";
 import SearchPage from "./pages/SearchPage";
 import SearchHistoryPage from "./pages/SearchHistoryPage";
+import NotFoundPage from "./pages/404";
 
 import { Footer } from "./components/Footer";
 import { Toaster } from "react-hot-toast";
@@ -40,7 +41,7 @@ export default function App() {
       <Route path="/watch/:id" element={user ? <WatchPage/> : <Navigate to={"/login"} />} />
       <Route path="/search" element={user ? <SearchPage/> : <Navigate to={"/login"} />} />
       <Route path="/history" element={user ? <SearchHistoryPage/> : <Navigate to={"/login"} />} />
-      <Route path="/history" element={user ? <SearchHistoryPage/> : <Navigate to={"/login"} />} />
+      <Route path="/*" element={<NotFoundPage />} />
 
     </Routes>
     <Toaster/>

64a1774f3387e520914108254ad285d5311484fa
Moabe <moabeluis2018@gmail.com>
2025-04-19 21:56:05 -0300

    Corrigindo erros de sixtaxe

--- Frontend/src/pages/SignUpPage.jsx
+++ Frontend/src/pages/SignUpPage.jsx
@@ -21,68 +21,67 @@ const SignUpPage = () => {
     <div className='h-screen w-full hero-bg'>
       <header className='max-w-6xl mx-auto flex items-center justify-between p-4'>
         <Link to={"/"}>
-          <img src="/netflix-logo.png" alt="logo"  className='w-52'/>
+          <img src="/netflix-logo.png" alt="logo" className='w-52' />
         </Link>
       </header>
 
       <div className='flex justify-center items-center mt-20 mx-3'>
-        <div className='w-full max-w-md p-8 space-x-6 bg-black/60 rounded-lg shadow-md'>
-          <h1 className=' text-center text-white text-2xl font-bold md-4'> Cadastre-se </h1>
+        <div className='w-full max-w-md p-8 space-y-6 bg-black/60 rounded-lg shadow-md'>
+          <h1 className='text-center text-white text-2xl font-bold mb-4'>Cadastre-se</h1>
 
           <form className='space-y-4' onSubmit={handleSignUp}>
             <div>
               <label htmlFor="email" className='text-sm font-medium text-gray-300 block'>
                 Email
               </label>
-              <input type="email"
-                className='w-full px-3 py-2 mt-1 border-gray-700 rounded-md bg-transparent text-white
-                focus:outline-none focus:ring'  
+              <input
+                type="email"
+                className='w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring'
                 placeholder='seu@exemplo.com'
                 id="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                
               />
             </div>
 
             <div>
               <label htmlFor="username" className='text-sm font-medium text-gray-300 block'>
-                Nome 
+                Nome
               </label>
-              <input type="username"
-                className='w-full px-3 py-2 mt-1 border-gray-700 rounded-md bg-transparent text-white
-                focus:outline-none focus:ring'  
+              <input
+                type="text"
+                className='w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring'
                 placeholder='Ex: João'
                 id="username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
               />
             </div>
-            
+
             <div>
-              <label htmlFor="paswword" className='text-sm font-medium text-gray-300 block'>
+              <label htmlFor="password" className='text-sm font-medium text-gray-300 block'>
                 Password
               </label>
-              <input type="password"
-                className='w-full px-3 py-2 mt-1 border-gray-700 rounded-md bg-transparent text-white
-                focus:outline-none focus:ring'  
+              <input
+                type="password"
+                className='w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring'
                 placeholder='Ex: 123456...'
                 id="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            <button className="w-full py-2 bg-red-600 text-white font-semibold rounded-md
-              hover:bg-red-700
-            ">
+
+            <button className="w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700">
               Cadastrar
             </button>
           </form>
+
           <div className="text-center text-gray-400">
-            Já é membro? {" "}
-              <Link to={"/login"} className="text-red-500 hover:undeline">
-                Entrar
-              </Link>
+            Já é membro?{" "}
+            <Link to={"/login"} className="text-red-500 hover:underline">
+              Entrar
+            </Link>
           </div>
         </div>
       </div>
